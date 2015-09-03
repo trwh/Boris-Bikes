febf4ebc220fd5f5d8ac1e37d79a068af3121366
gavin.w.cheng <gavin.w.cheng@gmail.com>
2015-09-03 16:28:21 +0100

    post challenge 2_v3

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,33 @@
 require_relative 'bike'
 
 class DockingStation
+	DEFAULT_CAPACITY = 20
+
+	attr_reader :capacity
+
+	def initialize
+		@bikes = []
+	  @capacity = DEFAULT_CAPACITY
+	end
+
 	def release_bike
-		fail "No bikes available" unless @bike
-    @bike
+		fail "No bikes available" if empty?
+    @bikes.pop
 	end
 
   def dock (bike)
-    fail "Docking station full" if @bike
-    @bike = bike
+    fail "Docking station full" if full?
+    @bikes.push bike
   end
+
+  private
+
+	def full?
+	  @bikes.count >= @capacity
+	end
+
+  def empty?
+	  @bikes.empty?
+	end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,8 +9,11 @@ describe DockingStation do
 		expect(subject.release_bike).to be_working
 	end
   it { expect(subject).to respond_to(:dock).with(1).argument }
+  it "has a default capacity" do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
 
-  describe "#release_bike" do
+	describe "#release_bike" do
     it "raises an error when there are no bikes available" do
       expect { subject.release_bike }.to raise_error "No bikes available"
     end
@@ -18,7 +21,7 @@ describe DockingStation do
 
   describe "#dock" do
     it "raises an error when full" do
-    	20.times { subject.dock(Bike.new) }
+    	subject.capacity.times { subject.dock(Bike.new) }
 			expect { subject.dock(Bike.new) }.to raise_error "Docking station full"
     end
   end
