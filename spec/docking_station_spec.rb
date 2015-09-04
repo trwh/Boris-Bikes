0d75d95def722a122bed30f3c76f716f3191cbb3
Tim Harrison <mail@trwh.co.uk>
2015-09-04 15:55:19 +0100

    End of Post Challenge 4

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,8 +10,7 @@ describe DockingStation do
   it { expect(subject).to respond_to :release_bike }
 
   it "releases working bikes" do
-    subject.dock Bike.new
-		# expect(subject.release_bike).to be_an_instance_of(Bike)
+    subject.dock (double :test_bike, working?: true)
 		expect(subject.release_bike).to be_working
 	end
 
@@ -29,22 +28,15 @@ describe DockingStation do
     end
 
     it "raises an error when only broken bikes are available" do
-      bike = Bike.new
-      bike.report_broken
-      subject.dock bike
+      subject.dock(double :broken_bike, working?: false)
       expect { subject.release_bike }.to raise_error "No bikes available"
     end
 
     it "returns only working bikes" do
-      working_bike = Bike.new
-
-      broken_bike = Bike.new
-      broken_bike.report_broken
-
       # Make a "working bike sandwich", to catch if a bike is just being taken from the start or end of the array.
-      subject.dock(broken_bike)
-      subject.dock(working_bike)
-      subject.dock(broken_bike)
+      subject.dock(double :broken_bike, working?: false)
+      subject.dock(double :working_bike, working?: true)
+      subject.dock(double :broken_bike, working?: false)
 
       expect(subject.release_bike).to be_working
     end
@@ -54,8 +46,8 @@ describe DockingStation do
   describe "#dock" do
 
     it "raises an error when full" do
-    	subject.capacity.times { subject.dock(Bike.new) }
-			expect { subject.dock(Bike.new) }.to raise_error "Docking station full"
+    	subject.capacity.times { subject.dock :test_bike }
+			expect { subject.dock :test_bike }.to raise_error "Docking station full"
     end
 
   end
