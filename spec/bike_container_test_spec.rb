0cf52430a6593a8c3be65efbaa307ae75d853ae2
Michael Roger <bodgeuk12@gmail.com>
2015-09-04 16:38:43 +0100

    Post Challenge 5 v1

---
+++ spec/bike_container_test_spec.rb
@@ -0,0 +1,14 @@
+
+require_relative '../lib/bike_container'
+require_relative './support/shared_examples_for_bike_container'
+
+# The purpose of this class is to give us an instance of
+# something that includes `BikeContainer`, to initially run tests against
+class BikeContainerTest
+  include BikeContainer
+end
+
+# `it_behaves_like BikeContainer` imports all the tests in the shared example
+describe BikeContainerTest do
+  it_behaves_like BikeContainer
+end
